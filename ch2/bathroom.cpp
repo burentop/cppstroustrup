e15829af762521c50d44f3c803f637726a2eeec1
Brent Perry <burentoperii@gmail.com>
2017-02-02 17:09:04 -0600

    finish chapter 2.

---
+++ ch2/bathroom.cpp
@@ -0,0 +1,12 @@
+#include "../std_lib_facilities.h"
+
+int main()
+{
+    cout << "Open the front door." << endl;
+    cout << "Walk 10 paces into the house." << endl;
+    cout << "Turn 90 degrees left." << endl;
+    cout << "Walk 12 paces toward the bathroom." << endl;
+    cout << "Open the bathroom door." << endl;
+    cout << "Enter the bathroom." << endl;
+    return 0;
+}
